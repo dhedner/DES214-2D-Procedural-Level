91f28a88c4703f41166ccc9098f9d6017f06abb0
David Hedner <dhedner@outlook.com>
2024-07-28 16:00:23 -0700

    add torches, spike logic, and animations

--- assets/scripts/spikes.gd
+++ assets/scripts/spikes.gd
@@ -7,10 +7,14 @@ enum State {
 
 @export var damage : int = 10
 
+@onready var damage_timer = $Timer
+
 var current_state: int = -1 : set = set_state
+var player_in_area: Node = null
 
 func _ready():
 	set_state(State.ACTIVE)
+	damage_timer.connect("timeout", _on_timer_timeout)
 
 func _process(delta):
 	pass
@@ -22,5 +26,17 @@ func set_state(new_state: int):
 	current_state = new_state
 
 func _on_body_entered(body):
-	if current_state == State.ACTIVE and body.has_method("handle_hit"):
-		body.handle_hit(damage)
+	if current_state == State.ACTIVE and body.name == "Player" and body.has_method("handle_hit"):
+		player_in_area = body
+		player_in_area.handle_hit(damage)
+		damage_timer.start()
+
+func _on_body_exited(body):
+	if body == player_in_area:
+		player_in_area = null
+		damage_timer.stop()
+
+func _on_timer_timeout():
+	if player_in_area and current_state == State.ACTIVE:
+		player_in_area.handle_hit(damage)
+
